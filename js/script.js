c2676fc97c5a4ec776ec0753df5667cabe43003d
Ev0zA <evoza47@gmail.com>
2025-11-13 17:53:58 +0300

    rgfdagsrg

---
+++ js/script.js
@@ -0,0 +1,29 @@
+const scrop = document.getElementById("scrip")
+
+const aug = (e) => {
+    scrip.addEventListener("click",(e) =>{
+    alert("Без ДДП, в родной краске, привизённая.")
+})
+}
+aug()
+    
+const au = (e) => {
+    crip.addEventListener("click",(e) =>{
+    alert("Без ДДП, в родной краске, привизённая.")
+})
+}
+au()
+
+const ab = (e) => {
+    crop.addEventListener("click",(e) =>{
+    alert("Без ДДП, в родной краске, от владельца.")
+})
+}
+ab()
+
+const abi = (e) => {
+    crops.addEventListener("click",(e) =>{
+    alert("#")
+})
+}
+abi()
